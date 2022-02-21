e09f095df353dd5ce8407a8e4f6eba43cbe19d67
Ben Lackey <ben.lackey@neo4j.com>
2022-02-21 09:16:08 -0800

    to 13

---
+++ form13/download.py
@@ -0,0 +1,15 @@
+import datetime
+import edgar
+
+
+def run():
+    date = datetime.date.today()
+
+    while date >= datetime.date(2021, 1, 1):
+        date = date - datetime.timedelta(days=1)
+        print(date)
+        edgar.downloadDate(date)
+        break
+
+
+run()
---
+++ form13/edgar.py
@@ -0,0 +1,64 @@
+import math
+import http.client
+import io
+import csv
+import form13
+#import database
+
+
+def downloadDate(date):
+    form13Paths = getForm13URLs(date)
+
+    # Download and parse each Form 13
+    print('We have ' + str(len(form13Paths)) + ' Form 13 URLs for the date ' + str(date))
+    t = []
+    #for path in form13Paths:
+    #    transactions = form13.download(path)
+    #    for transaction in transactions:
+    #        t.append(transaction)
+
+    #print('Done with download.  Writing to BigQuery...')
+    #db = database.database()
+    #db.insert(t)
+
+
+def getForm13URLs(date):
+    print('Composing the URL of the master file...')
+    year = str(date.year)
+    quarter = 'QTR' + str(math.ceil(date.month / 3))
+    date = date.strftime('%Y%m%d')
+    path = '/Archives/edgar/daily-index/' + year + '/' + quarter + '/master.' + date + '.idx'
+    url = 'https://www.sec.gov' + path
+    print('The URL of the master file is ' + url)
+
+    print('Downloading the master file...')
+    conn = http.client.HTTPSConnection('www.sec.gov')
+    conn.request('GET', path, headers={'User-Agent': 'Neo4j Ben.Lackey@Neo4j.com'})
+    response = conn.getresponse()
+    print(response.status, response.reason)
+    data = response.read()
+    conn.close()
+
+    if response.status == 200 and response.reason == 'OK':
+        text = data.decode('windows-1252')
+        form4Paths = parseMasterFile(text)
+        return form4Paths
+    else:
+        print('Download failed for master file.')
+        return []
+
+
+def parseMasterFile(text):
+    print('Parsing the master file...')
+    form4Paths = []
+    file = io.StringIO(text)
+    reader = csv.reader(file, delimiter='|')
+    for row in reader:
+        if len(row) != 5:
+            # This is a header
+            pass
+        elif row[2] == '13':
+            # This is a Form 13
+            form4Paths.append('/Archives/' + row[4])
+
+    return form4Paths
