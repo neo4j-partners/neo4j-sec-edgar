1366b36701a1ad81403b58c5dd72ca3650d0004a
Ben Lackey <ben.lackey@neo4j.com>
2022-02-22 20:04:13 -0800

    download 1 year of data

--- form13/download.py
+++ form13/download.py
@@ -4,8 +4,7 @@ import edgar
 
 def run():
     date = datetime.date.today()
-    date = datetime.date(2022, 2, 18)
-
+    
     while date >= datetime.date(2021, 1, 1):
         date = date - datetime.timedelta(days=1)
         print(date)
