28c75582e04302c218c8d728e5343934f62f116c
Ben Lackey <ben.lackey@neo4j.com>
2022-02-22 18:17:05 -0800

    parsign form 13

--- form13/form13.py
+++ form13/form13.py
@@ -1,3 +1,4 @@
+from importlib.resources import contents
 import xmltodict
 import http.client
 import io
@@ -41,10 +42,17 @@ def parse(file):
     contents = file.read()
     file.close()
 
-    # need to delete everything after: </ownershipDocument>
-    contents = contents.split('</XML>', 1)[0]
-    ownershipDocument = xmltodict.xmltodict(contents)
+    contents = contents.split('</XML>')
+    edgarSubmission = contents[0]
+    edgarSubmission = xmltodict.xmltodict(edgarSubmission)
+    print(edgarSubmission)
 
+    informationTable = contents[1]
+    informationTable = informationTable.split('<?xml version="1.0"?>')[1]
+    informationTable = xmltodict.xmltodict(informationTable)
+    print(informationTable)
+
+    '''
     issuerTradingSymbol = ownershipDocument['issuer'][0]['issuerTradingSymbol'][0]
     rptOwnerCik = ownershipDocument['reportingOwner'][0]['reportingOwnerId'][0]['rptOwnerCik'][0]
     rptOwnerName = ownershipDocument['reportingOwner'][0]['reportingOwnerId'][0]['rptOwnerName'][0]
@@ -54,68 +62,11 @@ def parse(file):
         isDirector = convertToBoolean(isDirector)
     except KeyError:
         isDirector = False
+    '''
+    
 
-    try:
-        isOfficer = ownershipDocument['reportingOwner'][0]['reportingOwnerRelationship'][0]['isOfficer'][0]
-        isOfficer = convertToBoolean(isOfficer)
-    except KeyError:
-        isOfficer = False
-
-    try:
-        isTenPercentOwner = ownershipDocument['reportingOwner'][0]['reportingOwnerRelationship'][0]['isTenPercentOwner'][0]
-        isTenPercentOwner = convertToBoolean(isTenPercentOwner)
-    except KeyError:
-        isTenPercentOwner = False
-
-    try:
-        isOther = ownershipDocument['reportingOwner'][0]['reportingOwnerRelationship'][0]['isOther'][0]
-        isOther = convertToBoolean(isOther)
-    except KeyError:
-        isOther = False
-
-    try:
-        ownershipDocuments = ownershipDocument['nonDerivativeTable'][0]['nonDerivativeTransaction']
-    except KeyError:
-        return []
-    except TypeError:
-        return []
-
-    transactions = []
-    i = 0
-    for nonDerivativeTransaction in ownershipDocuments:
-        try:
-            transactionDate = nonDerivativeTransaction['transactionDate'][0]['value'][0]
-            transactionShares = nonDerivativeTransaction['transactionAmounts'][0]['transactionShares'][0]['value'][0]
-            transactionPricePerShare = nonDerivativeTransaction['transactionAmounts'][0]['transactionPricePerShare'][0]['value'][0]
-            transactionAcquiredDisposedCode = nonDerivativeTransaction['transactionAmounts'][0]['transactionAcquiredDisposedCode'][0]['value'][0]
-            sharesOwned = nonDerivativeTransaction['postTransactionAmounts'][0]['sharesOwnedFollowingTransaction'][0]['value'][0]
-
-            if float(transactionPricePerShare) == 0:
-                pass
-            else:
-                transaction = {}
-                transaction['secDocument'] = secDocument + ' ' + str(i)
-                i += 1
-
-                transaction['acceptanceDatetime'] = acceptanceDatetime
-                transaction['issuerTradingSymbol'] = issuerTradingSymbol
-                transaction['rptOwnerCik'] = rptOwnerCik
-                transaction['rptOwnerName'] = rptOwnerName
-                transaction['isDirector'] = isDirector
-                transaction['isOfficer'] = isOfficer
-                transaction['isTenPercentOwner'] = isTenPercentOwner
-                transaction['isOther'] = isOther
-                transaction['transactionDate'] = transactionDate
-                transaction['transactionShares'] = float(transactionShares)
-                transaction['transactionPricePerShare'] = float(transactionPricePerShare)
-                transaction['transactionAcquiredDisposedCode'] = transactionAcquiredDisposedCode
-                transaction['sharesOwned'] = float(sharesOwned)
-                transactions.append(transaction)
-
-        except KeyError:
-            pass
-
-    return transactions
+    filings = []
+    return filings
 
 
 def convertToBoolean(s):
