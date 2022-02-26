4c18033df7150ab9accd3c52a4620aaf6a436ea7
Ben Lackey <ben.lackey@neo4j.com>
2022-02-25 18:17:50 -0800

    drop break

--- form13/download.py
+++ form13/download.py
@@ -4,11 +4,8 @@ import edgar
 
 def run():
     date = datetime.date.today()
-    date = datetime.date(2022, 2, 18) 
-
     while date >= datetime.date(2021, 1, 1):
         date = date - datetime.timedelta(days=1)
         edgar.downloadDate(date)
-        break
 
 run()
