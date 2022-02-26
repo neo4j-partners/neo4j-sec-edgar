0f596a1aa4db682278b5c282d41afd02b1888d12
Ben Lackey <ben.lackey@neo4j.com>
2022-02-25 19:18:50 -0800

    one match

--- form13/form13.py
+++ form13/form13.py
@@ -44,7 +44,7 @@ def parse(file):
     informationTable = contents[2]
     informationTable = informationTable.split('</XML>')[0]
     informationTable = informationTable.split('\n',1)[1]
-    informationTable = informationTable.replace('" http', '"http')
+    informationTable = informationTable.replace('" http', '"http',1)
     try:
         informationTable = xmltodict.xmltodict(informationTable)
     except:
