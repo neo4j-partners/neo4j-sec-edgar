2b39d8daa7bfdbe42f7b201d7584e965b7a29cb0
Ben Lackey <ben.lackey@neo4j.com>
2022-02-28 14:17:05 -0800

    print

--- featurize/featurize.py
+++ featurize/featurize.py
@@ -3,6 +3,7 @@ import pandas
 
 form13 = pandas.DataFrame()
 for file in listdir('../data/'):
+    print('Processing file: ' + file)
     form13.append(pandas.read_csv('../data/' + file))
 
 print(form13)
