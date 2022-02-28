02a90cfd51db4fe5d1eb3d2433f218febeb07a2e
Ben Lackey <ben.lackey@neo4j.com>
2022-02-28 15:19:42 -0800

    target computations

--- featurize/featurize.py
+++ featurize/featurize.py
@@ -16,14 +16,33 @@ df.replace('3-31-2021', '03-31-2021', inplace=True)
 # So, the only data we want is from: '03-31-2021', '06-30-2021', '09-30-2021', '12-31-2021'
 df = df.loc[df['reportCalendarOrQuarter'].str.contains('2021')]
 
+print('Load complete.  Computing targets...')
+df['target']=False
+df = df.reset_index()
+for index, row in df.iterrows():
+    reportCalendarOrQuarter = row['reportCalendarOrQuarter']
+    filingManager = row['filingManager']
+    cusip = row['cusip']
+    shares = row['shares']
 
+    if reportCalendarOrQuarter == '03-31-2021':
+        targetReportCalendarOrQuarter = '06-30-2021'
+    elif reportCalendarOrQuarter == '06-30-2021':
+        targetReportCalendarOrQuarter = '09-30-2021'
+    elif reportCalendarOrQuarter == '09-30-2021':
+        targetReportCalendarOrQuarter = '12-31-2021'
+    elif reportCalendarOrQuarter == '12-31-2021':
+        targetReportCalendarOrQuarter = '03-31-2022'
 
+    targetRow = df.loc[(df['reportCalendarOrQuarter'] == targetReportCalendarOrQuarter) & (df['filingManager'] == filingManager) & (df['cusip'] == cusip)]
+    # We're assuming uniqueness here where it may not be valid.
+    # Probably need to go back and ensure we don't download revised filings.
+    targetShares = targetRow.head(1)['shares']
 
+    if targetShares > shares:
+        df.loc[index, 'target'] = True
 
-
-
-
-# split the data
+print('Splitting data and writing files to disk...')
 train = df.loc[df['reportCalendarOrQuarter'] == '03-31-2021']
 train = train.append(df.loc[df['reportCalendarOrQuarter'] == '06-30-2021'])
 test = df.loc[df['reportCalendarOrQuarter'] == '09-30-2021']
